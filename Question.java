019b12622a47367c6e9d4d2b548fc00e2d973812
Muhammad Firman Kahfi <firman.mfk@gmail.com>
2017-05-13 00:26:16 +0700

    Update Question.java

--- Question.java
+++ Question.java
@@ -8,86 +8,89 @@ import android.app.Activity;
 
 public class Question extends Activity {
 
+	private int ID;
+	private String QUESTION;
+	private String OPTA;
+	private String OPTB;
+	private String OPTC;
 
-        private int ID;
-        private String QUESTION;
-        private String OPTA;
-        private String OPTB;
-        private String OPTC;
+	private String ANSWER;
+	
 
-        private String ANSWER;
+	public Question() {
+		ID = 0;
+		QUESTION = "";
+		OPTA = "";
+		OPTB = "";
+		OPTC = "";
+
+		ANSWER = "";
+
+	}
+
+	public Question(String qUESTION, String oPTA, String oPTB, String oPTC,
+			String aNSWER) {
+		QUESTION = qUESTION;
+		OPTA = oPTA;
+		OPTB = oPTB;
+		OPTC = oPTC;
+
+		ANSWER = aNSWER;
+
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public String getQUESTION() {
+		return QUESTION;
+	}
+
+	public String getOPTA() {
+		return OPTA;
+	}
+
+	public String getOPTB() {
+		return OPTB;
+	}
+
+	public String getOPTC() {
+		return OPTC;
+	}
+
+	public String getANSWER() {
+		return ANSWER;
+	}
+
+	public void setID(int id) {
+		ID = id;
+	}
+
+	public void setQUESTION(String qUESTION) {
+		QUESTION = qUESTION;
+	}
+
+	public void setOPTA(String oPTA) {
+		OPTA = oPTA;
+	}
+
+	public void setOPTB(String oPTB) {
+		OPTB = oPTB;
+	}
+
+	public void setOPTC(String oPTC) {
+		OPTC = oPTC;
+	}
+
+	public void setANSWER(String aNSWER) {
+		ANSWER = aNSWER;
+	}
+
+}
 
 
-        public Question() {
-            ID = 0;
-            QUESTION = "";
-            OPTA = "";
-            OPTB = "";
-            OPTC = "";
 
-            ANSWER = "";
-
-        }
-
-        public Question(String qUESTION, String oPTA, String oPTB, String oPTC,
-                        String aNSWER) {
-            QUESTION = qUESTION;
-            OPTA = oPTA;
-            OPTB = oPTB;
-            OPTC = oPTC;
-
-            ANSWER = aNSWER;
-
-        }
-
-        public int getID() {
-            return ID;
-        }
-
-        public void setID(int ID) {
-            this.ID = ID;
-        }
-
-        public String getQUESTION() {
-            return QUESTION;
-        }
-
-        public void setQUESTION(String QUESTION) {
-            this.QUESTION = QUESTION;
-        }
-
-        public String getOPTA() {
-            return OPTA;
-        }
-
-        public void setOPTA(String OPTA) {
-            this.OPTA = OPTA;
-        }
-
-        public String getOPTB() {
-            return OPTB;
-        }
-
-        public void setOPTB(String OPTB) {
-            this.OPTB = OPTB;
-        }
-
-        public String getOPTC() {
-            return OPTC;
-        }
-
-        public void setOPTC(String OPTC) {
-            this.OPTC = OPTC;
-        }
-
-        public String getANSWER() {
-            return ANSWER;
-        }
-
-        public void setANSWER(String ANSWER) {
-            this.ANSWER = ANSWER;
-        }
-    }
 
 // @Override
 // public boolean onCreateOptionsMenu(Menu menu) {
