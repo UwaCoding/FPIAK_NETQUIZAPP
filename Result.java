fcfef71c5282822ea1c69393da571b0e771c046a
Muhammad Firman Kahfi <firman.mfk@gmail.com>
2017-05-13 00:31:54 +0700

    Update Result.java

--- Result.java
+++ Result.java
@@ -9,25 +9,27 @@ import android.widget.TextView;
 
 public class Result extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_result);
 
-        TextView textResult = (TextView) findViewById(R.id.textResult);
+		TextView textResult = (TextView) findViewById(R.id.textResult);
 
-        Bundle b = getIntent().getExtras();
+		Bundle b = getIntent().getExtras();
 
-        int score = b.getInt("score");
+		int score = b.getInt("score");
 
         textResult.setText("Your score is " + " " + score + ". Thanks for playing my game.");
-    }
-    public void playagain(View o) {
 
-        Intent intent = new Intent(this, QuestionActivity.class);
+	}
 
-        startActivity(intent);
+	public void playagain(View o) {
+
+			Intent intent = new Intent(this, QuestionActivity.class);
+
+			startActivity(intent);
 
 
-    }
+	}
 }
